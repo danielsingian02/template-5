d652a1cc5a522a4de920de00f8ba81ced909e87e
Daniel Singian <daniel.singian02@gmail.com>
2022-12-19 18:10:58 +0800

    added hello world

--- index.php
+++ index.php
@@ -2,6 +2,8 @@
 /**
  * The main template file
  *
+ * Hello World
+ *
  * This is the most generic template file in a WordPress theme
  * and one of the two required files for a theme (the other being style.css).
  * It is used to display a page when nothing more specific matches a query.
