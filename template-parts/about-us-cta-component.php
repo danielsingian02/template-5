1efb5e5f943f8a5b3a11f42542b9adf63b89f381
Daniel Siningian <daniel.singian02@gmail.com>
2023-04-11 18:33:04 +0800

    clean code, namespace resolved.

--- template-parts/about-us-cta-component.php
+++ template-parts/about-us-cta-component.php
@@ -1,24 +1,19 @@
-<?php
-?>
-
-<div class="about-us-call-to-action__container"
-    style="background-color: <?php echo $args[ 'bg_color' ]; ?>"
->
-	<div class="about-us-call-to-action__wrapper">
-		<h3 class="about-us-cta__title" style="color: <?php echo $args[ 'text_color' ]; ?>">
-			<?php
-			echo $args["title"];
-			?>
-		</h3>
-		<div class="about-us-cta__content" style="color: <?php echo $args[ 'text_color' ]; ?>">
-			<?php
-			echo $args["content"];
-			?>
-		</div>
-		<a class="about-us-cta__button" style="background-color: <?php echo $args[ 'button_background_color' ]; ?>; color: <?php echo $args[ 'button_text_color' ]; ?>" target="_blank" href="<?php echo $args["button_link"]; ?>">
-			<?php
-			echo $args["button_text"];
-			?>
-		</a>
-	</div>
-</div>
+ <div class="about-us-call-to-action__container" style="background-color: <?php echo $args['bg_color']; ?>">
+ 	<div class="about-us-call-to-action__wrapper">
+ 		<h3 class="about-us-cta__title" style="color: <?php echo $args['text_color']; ?>">
+ 			<?php
+				echo $args["title"];
+				?>
+ 		</h3>
+ 		<div class="about-us-cta__content" style="color: <?php echo $args['text_color']; ?>">
+ 			<?php
+				echo $args["content"];
+				?>
+ 		</div>
+ 		<a class="about-us-cta__button" style="background-color: <?php echo $args['button_background_color']; ?>; color: <?php echo $args['button_text_color']; ?>" target="_blank" href="<?php echo $args["button_link"]; ?>">
+ 			<?php
+				echo $args["button_text"];
+				?>
+ 		</a>
+ 	</div>
+ </div>
--- template-parts/welcome-component.php
+++ template-parts/welcome-component.php
@@ -10,25 +10,25 @@
         </h2>
 
         <div class="welcome-component__content__content">
-		    <?php echo $args['content']; ?>
+            <?php echo $args['content']; ?>
         </div>
 
         <!-- List -->
         <?php
-            if ($args['display_list']) {
-	            echo "<div class='welcome-component__content__lists' style='display: flex'>";
+        if ($args['display_list']) {
+            echo "<div class='welcome-component__content__lists' style='display: flex'>";
 
-                    foreach ( $args[ "lists" ] as $list ){
-                        if( $list = acf_esc_htmL( $list ) ) {
-	                        echo "<div class='welcome-component__content__lists__list'>";
-                                echo "<i class='bi bi-check-circle-fill'></i>";
-                                echo "<p>$list</p>";
-	                        echo "</div>";
-                        }
-                    }
-
-                echo "</div>";
+            foreach ($args["lists"] as $list) {
+                if ($list = acf_esc_htmL($list)) {
+                    echo "<div class='welcome-component__content__lists__list'>";
+                    echo "<i class='bi bi-check-circle-fill'></i>";
+                    echo "<p>$list</p>";
+                    echo "</div>";
+                }
             }
+
+            echo "</div>";
+        }
         ?>
 
     </div>
@@ -37,17 +37,15 @@
             <div class="grid-image__left">
                 <img src="
                     <?php
-                        echo $args['left_image'] ?: "https://raw.githubusercontent.com/danielsingian02/template_5/main/assets/images/Asset2.png";
+                    echo $args['left_image'] ?: "https://raw.githubusercontent.com/danielsingian02/template_5/main/assets/images/Asset2.png";
                     ?>
-                "
-                     alt="">
+                " alt="">
             </div>
             <div class="grid-image__right">
                 <img src="
                     <?php
                     echo $args['right_image'] ?: "https://raw.githubusercontent.com/danielsingian02/template_5/main/assets/images/Asset3.png";
-                    ?>"
-                     alt="">
+                    ?>" alt="">
             </div>
         </div>
     </div>
